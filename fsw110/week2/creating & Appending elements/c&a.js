51460980bbf4c35b8ed5fcbf139a7d45d8a260f2
sarahmarie333 <you@example.com>
2020-04-18 19:17:00 -0700

    append

---
+++ fsw110/week2/creating & Appending elements/c&a.js
@@ -0,0 +1,14 @@
+
+
+  var node = document.createElement("LI");
+  var textnode = document.createTextNode("Water");
+  var textnode = document.createTextNode("Coffe");
+  var textnode = document.createTextNode("Tea");
+  node.appendChild(textnode);
+  document.getElementById("myList").appendChild(node);
+
+  var h = document.createElement("H1");                
+  var t = document.createTextNode("Welcome to my JS Site");     
+  h.appendChild(t); 
+  document.getElementById("H1").appendChild(h);
+
