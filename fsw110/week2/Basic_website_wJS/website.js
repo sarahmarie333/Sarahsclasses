8ef9b1257075d6326d05231525b29dc3d7a6f6f5
sarahmarie333 <you@example.com>
2020-04-19 18:24:46 -0700

    basicWebSite

---
+++ fsw110/week2/Basic_website_wJS/website.js
@@ -0,0 +1,25 @@
+function myFunction() {
+    var x = document.createElement("HEADER");
+    x.setAttribute("id", "myHeader");
+    document.body.appendChild(x);
+  
+      function myFunction() {
+      var node = document.createElement("LI");
+      var textnode = document.createTextNode("coffee");
+      var textnode = document.createTextNode("milk");
+      var textnode = document.createTextNode("eggs");
+      node.appendChild(textnode);
+      document.getElementById("myList").appendChild(node);
+    }
+    function myFunction() {
+      var node = document.createElement("FOOTER");
+      var textnode = document.createTextNode("this is the footer");
+      node.appendChild(textnode);
+      document.getElementById("myf").appendChild(node);
+    }
+  
+    document.getElementById("myHeader").appendChild(y);
+  }
+document.getElementById("myH").innerHTML = "JavaScript Website";
+
+document.getElementById("myP").innerHTML = "This Is A Paragraph.";
