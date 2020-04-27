9f912fc9d9e6c78c7a8edec5614f27a5c1ed2e4a
sarahmarie333 <you@example.com>
2020-04-26 22:39:42 -0700

    JS styles

---
+++ fsw110/week3/changing_styles_wJS/styles.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    document.getElementById("myDIV").classList.add("style");
+    document.getElementById("mydiv2").classList.add("border");
+  }
+
+  
+
+document.body.style.color = "cornflowerblue";
+document.body.style.fontweight ="lighter";
+document.body.style.fontSize = "20px";
+document.body.style.fontfamily = "sans-serif";
