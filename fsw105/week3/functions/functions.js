a2564e4cb01da3e177f7b7b4f96da07e4ddaf7a0
sarahmarie333 <you@example.com>
2020-05-02 12:32:59 -0700

    resubmit

--- fsw105/week3/functions/functions.js
+++ fsw105/week3/functions/functions.js
@@ -1,39 +1,30 @@
-function sum(num1, num2){
+function mySum(num1, num2){
     return num1 + num2
 }
 
 
-var result = sum(10, 25) 
-console.log(result);
+console.log (mySum (10, 25));
 
-function sum(num1, num2){
-    return num1 + num2 
+function nums3(one, two, three){
+  return Math.max(one, two, three)
 }
-
-
-var result = sum(10, 25, 40) 
-console.log(result)
-
-function sum(num1, num2, num3){
-    return num1 + num2 + num3
-}
-
-var result = sum(10) 
-console.log(result);
+console.log (nums3(1, 2, 3));
 
 function oddOrEven(sum){
-    if(sum % 2 == 0)
-        return "even";
-    return "odd";
+    if(sum % 2 === 0){
+        return "even";}
+    
+    else {return "odd";} 
 }
-oddOrEven(sum);
+console.log (oddOrEven(2));
+console.log (oddOrEven(3));
 
-var result = sum(34) 
-console.log(result);
+
+var result4 = mySum(34) 
 
 function myFunction(x, y) {
-    if (sum > 20) {
-      y = 0;
+    if (mySum > 20) {
     }  
     return x * y;
-  }
+}
+  
